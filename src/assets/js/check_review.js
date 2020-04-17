c92f8b4d2e922e9f8d8b0c4f364d9cc22f3984bf
ahmed-debbech <debbech.ahmed@gmail.com>
2020-04-17 20:03:56 +0100

    Add check for front even review data

---
+++ src/assets/js/check_review.js
@@ -0,0 +1,29 @@
+function check(){
+    document.getElementById("submit").addEventListener("click", function(event){
+        event.preventDefault()
+      });
+    let title = document.getElementById("title");
+    let text = document.getElementById("text");
+    let stars = document.getElementsByName("review-rate");
+    if(title.value.length <= 0){
+        alert("Title should not be empty!");
+        return false;
+    }else{
+        if(text.value.length <= 0){
+            alert("Review should not be empty!");
+            return false;
+        }else{
+            let count =0;
+            for(let i=0; i<5; i++){
+                if(stars[i].checked == true){
+                    count++;
+                }
+            }
+            if(count == 0){
+                alert("You should give at least one star!");
+                return false;
+            }
+        }
+    }
+    return true;
+}
