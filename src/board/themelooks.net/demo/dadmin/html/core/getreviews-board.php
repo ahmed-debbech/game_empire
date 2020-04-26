5dd5beba4fddae25fb3110b7618b33b3082f6395
ahmed-debbech <debbech.ahmed@gmail.com>
2020-04-26 20:43:26 +0100

    Show reviews in list section in dashboard

---
+++ src/board/themelooks.net/demo/dadmin/html/core/getreviews-board.php
@@ -0,0 +1,50 @@
+<?php
+include("../../../../../config.php");
+function retrive(){
+    $db = config::getConnexion();
+    $sql = "select * from reviews";
+    try{
+        $list = $db->query($sql);
+    }catch(Exception $e){
+        echo "error";
+    }
+    $res = array();
+        foreach($list as $row){
+                array_push($res, $row);
+        }
+        return $res;
+}
+function convertStars($x){
+    switch($x){
+        case 1: return 20;
+        break;
+        case 2: return 40;
+        break;
+        case 3: return 60;
+        break;
+        case 4: return 80;
+        break;
+        case 5: return 100;
+        break;
+    }
+}
+$j=0;
+$list = retrive();
+for($i=sizeof($list)-1; $i>=0; $i--){
+    echo "<tr>
+        <td> <a class='btn-link'>".$list[$i]["id_rev"]."</a> </td>
+        <td> <span class='label label-success'><a class='btn-link'>".$list[$i]["username"]."</a></span>  </td>
+        <td> <a class='btn-link'>".convertStars($list[$i]["id_rev"])."</a> </td>
+        <td>".$list[$i]["content"]."</td>
+        <td>".$list[$i]["date"]."</td>
+        <td>".$list[$i]["title"]."</td>
+        <td> <span class='label label-success'>".$list[$i]["id_game"]."</span> </td>
+        <td>
+            <div class='dropleft'> <a class='btn-link' data-toggle='dropdown'><i class='fa fa-ellipsis-v'></i></a>
+                <div class='dropdown-menu'> <a class='dropdown-item'>Delete</a> <a class='dropdown-item'></a> </div>
+            </div>
+        </td>
+    </tr>";
+    $j++;
+}
+?>
